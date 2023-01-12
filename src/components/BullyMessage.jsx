6b85fbd79f615797796b1b1e9b8cc6fcaf12b89a
Choy050823 <choymh23@gmail.com>
2023-01-13 00:16:50 +0800

    prevent alert box to prompt forever

--- src/components/BullyMessage.jsx
+++ src/components/BullyMessage.jsx
@@ -4,21 +4,24 @@ import { useState } from "react";
 const BullyMessage = () => {
     const [bullyMsg, SetBullyMsg] = useState("Normal");
     let ws = new WebSocket("ws://localhost:4896");
+    var isNotified = false;
 
     ws.onopen = function (e) {
       console.log("opened");
     };
     ws.onmessage = function (e) {
       console.log(e.data);
-      if (e.data === "bully_detected") {
+      if (e.data === "bully_detected" && !isNotified) {
         ws.send("receive_ok");
         console.log("[SERVER] BULLY DETECTED");
         // bullyStatus = "BULLY DETECTED";
         // console.log(bullyStatus);
         SetBullyMsg("Bully Detected");
         alert("Bully Detected in CCTV! \n Please Respond Immediately!");
+        isNotified = true;
       } else {
         SetBullyMsg("Normal");
+        isNotified = false;
       }
     };
     ws.onclose = function (e) {
